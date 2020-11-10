4e75a1a879f73bc2cd31856971538c4a77fd5e6d
Richard Walsh <richwalsh889@gmail.com>
2020-11-10 16:09:24 +0000

    add animation to todo card and add styling to scrollbar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import "./scss/main.scss";
 console.log('hello there')
+
+var test = document.getElementsByClassName('btn__collapse');
+
+console.log(test)
+
+for (let i = 0; i < test.length; i++) {
+    test[i].addEventListener("click", function() {
+    //   this.classList.toggle("active");
+      var content = document.querySelector('.todo-card__notes');
+      var content2 = document.querySelector('.todo-card__checklist');
+      var content3 = document.querySelector('.btn__collapse');
+
+      if (content.style.display === "block" && content2.style.display === "block") {
+        content.style.display = "none";
+        content2.style.display = "none";
+
+    
+      } else {
+        content.style.display = "block";
+        content2.style.display = "block";
+        content3.style.order = "3"
+    
+      }
+    });
+  }
