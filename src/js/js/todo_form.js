c661d870a48284421f7cc72091eddf041199ada7
Richard Walsh <richwalsh2@googlemail.com>
2021-08-13 20:19:06 +0100

    fix duplicate todo bug

--- src/js/js/todo_form.js
+++ src/js/js/todo_form.js
@@ -1,84 +1,81 @@
-import {todoFactory} from './factory'
-import {newDate, removeChild} from './utilities'
-import {todoArr, setLocalStorage} from './data'
-import {render} from './render';
-
+import { todoFactory } from './factory';
+import { newDate, removeChild } from './utilities';
+import { todoArr, setLocalStorage } from './data';
+import { render } from './render';
 
 // let todoArr = [{id: '1111', title: 'test', description: 'this is the first todo', dueDate: newDate(2019, 11, 2), priority: 4, notes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus incidunt', complete: true, completeDate: newDate(2019, 11, 8)},
 // {id: '1112', title: 'second Todo', description: 'this is the second todo', dueDate: newDate(2020, 11, 8), priority: 5, notes: 'inventore exercitationem ipsam tenetur veniam natus repellat nemo cupiditate!',  complete: false},
 //     {id: '1113', title: 'third Todo', description: 'this is the third todo', dueDate: newDate(2020, 11, 1), priority: 4, notes: 'inventore exercitationem ipsam tenetur veniam natus repellat nemo cupiditate!' , complete: false},]
 // let todoArr = []
 
-const submitForm = (event) => {
-    let title, description, dueDate, priority, notes, checkList, project, JSDate
-    
-    const radioChild = document.getElementById('form__radio')
-    const newProject = document.getElementById('todo-form__new')
-    
-    title = document.getElementById('todo-form__title').value;
-    description = document.getElementById('todo-form__description').value;
-    dueDate = document.getElementById('todo-form__due-date').value;
-    priority = 4;
-    notes = document.getElementById('todo-form__notes').value;
-    checkList = document.getElementById('checklist');
-    
-    
-    if (newProject.checked === true) {
-        project = document.getElementById('todo-form__new--text').value.toUpperCase()
-    } else if (radioChild.hasChildNodes()){
-        console.log(radioChild.childNodes)
-        radioChild.childNodes.forEach( function(element) {
-            if (element.type === 'radio' && element.checked === true) {
-                            project = document.getElementById(element.id).value.toUpperCase()
-                        } 
-        })
-    }
+const submitForm = () => {
+  let title, description, dueDate, priority, notes, checkList, project, JSDate;
 
-    // if (radioChild.hasChildNodes()){
-    //     console.log(radioChild.childNodes)
-    //     radioChild.childNodes.forEach( function(element) {
-    //         if (element.type === 'radio' && element.id === 'todo-form__new' && element.checked === true) {
-    //             project = document.getElementById('todo-form__new--text').value
-    //         } else if (element.type === 'radio' && element.checked === true) {
-    //             project = document.getElementById(element.id).value
-    //         } 
-    //     })}
+  const radioChild = document.getElementById('form__radio');
+  const newProject = document.getElementById('todo-form__new');
 
+  title = document.getElementById('todo-form__title').value;
+  description = document.getElementById('todo-form__description').value;
+  dueDate = document.getElementById('todo-form__due-date').value;
+  priority = 4;
+  notes = document.getElementById('todo-form__notes').value;
+  checkList = document.getElementById('checklist');
 
-    todoArr.push(todoFactory(title, description, dueDate, priority, notes, checkList, project))
-    removeChild('section-todo')
-    
-    setLocalStorage()
-    render();
-}
+  if (newProject.checked === true) {
+    project = document
+      .getElementById('todo-form__new--text')
+      .value.toUpperCase();
+  } else if (radioChild.hasChildNodes()) {
+    radioChild.childNodes.forEach(function (element) {
+      if (element.type === 'radio' && element.checked === true) {
+        project = document.getElementById(element.id).value.toUpperCase();
+      }
+    });
+  }
+
+  todoArr.push(
+    todoFactory(
+      title,
+      description,
+      dueDate,
+      priority,
+      notes,
+      checkList,
+      project
+    )
+  );
+  removeChild('section-todo');
+
+  setLocalStorage();
+  render();
+};
 
 const addStep = (validate) => {
+  let newStep = document.getElementById('todo-form__checklist').value;
+  const element = document.getElementById('checklist');
 
-    let newStep = document.getElementById('todo-form__checklist').value;
-    const element = document.getElementById('checklist')
+  const newP = document.createElement('p');
 
-    const newP = document.createElement('p');
-    
-    if (validate === true) {
-        console.log(validate)
-        newStep = 'This ToDo does not contain any additional steps'
-    }
+  if (validate === true) {
+    console.log(validate);
+    newStep = 'This ToDo does not contain any additional steps';
+  }
 
-    const newContent = document.createTextNode(newStep);
-    console.log(newStep)
-    newP.appendChild(newContent);
+  const newContent = document.createTextNode(newStep);
+  console.log(newStep);
+  newP.appendChild(newContent);
 
-    element.insertAdjacentElement("beforeend", newP)
+  element.insertAdjacentElement('beforeend', newP);
 
-    console.log(element.childNodes[0].value)
-}
+  console.log(element.childNodes[0].value);
+};
 
 const closeForm = () => {
-    const formOpen = document.getElementById('form-toggle')
+  const formOpen = document.getElementById('form-toggle');
 
-    if (formOpen.checked = true) {
-        formOpen.checked = false
-    }
-}
+  if ((formOpen.checked = true)) {
+    formOpen.checked = false;
+  }
+};
 
-export { todoArr, submitForm, addStep, closeForm }
+export { todoArr, submitForm, addStep, closeForm };
--- src/js/js/validation.js
+++ src/js/js/validation.js
@@ -1,78 +1,83 @@
-import { add } from 'date-fns';
-import { addStep, submitForm} from './todo_form';
-import { closeForm } from './todo_form'
-import {removeChild} from './utilities'
+import { addStep, submitForm, closeForm } from './todo_form';
+import { removeChild } from './utilities';
+
+// DOM STRINGS
+const domStrings = {
+  todoTitle: document.getElementById('todo-form__title'),
+  tododescription: document.getElementById('todo-form__description'),
+  todoDueDate: document.getElementById('todo-form__due-date'),
+  todoNotes: document.getElementById('todo-form__notes'),
+  todoChecklist: document.getElementById('todo-form__checklist'),
+  todoNewProject: document.getElementById('todo-form__new--text'),
+  todoNewProjectInput: document.getElementById('todo-form__new--text'),
+  validationWarning: document.getElementById('validation-warning'),
+  validationWarningContent: document.getElementById(
+    'validation-warning__content'
+  ),
+};
+
+const validateAlert = (inputWarning) => {
+  // style.display sets inline style not style in css file
+  if (
+    domStrings.validationWarning.style.display === 'none' ||
+    domStrings.validationWarning.style.display === ''
+  ) {
+    domStrings.validationWarning.style.display = 'block';
+    domStrings.validationWarningContent.textContent = `Please input a ${inputWarning} for the todo`;
+    setTimeout(() => {
+      domStrings.validationWarning.style.display = 'none';
+    }, 3000);
+  }
+};
+
+const validateContent = (element, defaultContent) => {
+  const el = element;
+  el.value = `This ToDo does not have any additional ${defaultContent}`;
+};
+
+const clearForm = () => {
+  // submitForm(event);
+  closeForm();
+
+  Object.entries(domStrings).forEach(([key, value]) => {
+    const formValue = value;
+    formValue.value = '';
+  });
+
+  removeChild('checklist');
+};
 
 const validateForm = (event) => {
-    // DOM STRINGS
+  // const validateAlert = (inputWarning) => {
+  //     alert(`Please input a ${inputWarning} for the todo`)
+  // }
 
-    let domStrings = {
-        todoTitle: document.getElementById('todo-form__title'),
-        tododescription: document.getElementById('todo-form__description'),
-        todoDueDate: document.getElementById('todo-form__due-date'),
-        todoNotes: document.getElementById('todo-form__notes'),
-        todoChecklist: document.getElementById('todo-form__checklist'),
-        todoNewProject: document.getElementById('todo-form__new--text'),
-        todoNewProjectInput: document.getElementById('todo-form__new--text'),
-        validationWarning : document.getElementById('validation-warning'),
-        validationWarningContent : document.getElementById('validation-warning__content'),
-
-    }
-
-    // const validateAlert = (inputWarning) => {
-    //     alert(`Please input a ${inputWarning} for the todo`)
-    // }
-
-    const validateAlert = (inputWarning) => {
-        // style.display sets inline style not style in css file
-        if (domStrings.validationWarning.style.display === 'none' || domStrings.validationWarning.style.display === '') {
-            console.log('validatewarning if' )
-            domStrings.validationWarning.style.display = 'block'
-            domStrings.validationWarningContent.textContent = `Please input a ${inputWarning} for the todo`
-            setTimeout(() => {
-                domStrings.validationWarning.style.display = 'none'
-            }, 3000);
-        }
-    }
-
-    const validateContent = (element, defaultContent) => {
-        element.value = `This ToDo does not have any additional ${defaultContent}`
-    }
-
-    const clearForm = (domStrings) => {
-        submitForm(event)
-        closeForm()
-        
-        Object.entries(domStrings).forEach(
-            ([key, value]) => value.value = ''
-        );
-        
-        removeChild('checklist')
-        
-    }
-
-    if (domStrings.todoTitle.value === '') {
-        validateAlert('title')
-    } else if (domStrings.tododescription.value === '') {
-        validateAlert('description')
-    } else if (domStrings.todoDueDate.value === '') {
-        validateAlert('duedate');
-    } else if (domStrings.todoNotes.value === '' && domStrings.todoChecklist.value === '') {
-        validateContent(domStrings.todoNotes, 'notes')
-        addStep(true)
-        clearForm(domStrings)
-    } else if (domStrings.todoNotes.value === '') {
-        validateContent(domStrings.todoNotes, 'notes');
-        clearForm(domStrings)
-    } else if (domStrings.todoChecklist.value === '') {
-        addStep(true)
-        clearForm(domStrings)
-    } else {
-        submitForm(event)
-        clearForm(domStrings)
-}
-    }
-
-    
+  if (domStrings.todoTitle.value === '') {
+    validateAlert('title');
+  } else if (domStrings.tododescription.value === '') {
+    validateAlert('description');
+  } else if (domStrings.todoDueDate.value === '') {
+    validateAlert('duedate');
+  } else if (
+    domStrings.todoNotes.value === '' &&
+    domStrings.todoChecklist.value === ''
+  ) {
+    validateContent(domStrings.todoNotes, 'notes');
+    addStep(true);
+    submitForm();
+    clearForm();
+  } else if (domStrings.todoNotes.value === '') {
+    validateContent(domStrings.todoNotes, 'notes');
+    submitForm();
+    clearForm();
+  } else if (domStrings.todoChecklist.value === '') {
+    addStep(true);
+    submitForm();
+    clearForm();
+  } else {
+    submitForm(event);
+    clearForm();
+  }
+};
 
 export default validateForm;
